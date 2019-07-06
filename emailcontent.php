f14dda6750f2267ba25bbb353c65c853392d0a7c
RubixSolver99 <blackbattiger@outlook.com>
2019-07-06 17:26:12 -0500

    Testing emailcontent with php

---
+++ emailcontent.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+  <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+  <title>Furious Falcons Email</title>
+  <style type="text/css">
+    /* Some resets and issue fixes */
+    #outlook a { padding:0; }
+    body{ width:100% !important; -webkit-text; size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0; }
+    .ReadMsgBody { width: 100%; }
+    .ExternalClass {width:100%;}
+    .backgroundTable {margin:0 auto; padding:0; width:100%;!important;}
+    table td {border-collapse: collapse;}
+    .ExternalClass * {line-height: 115%;}
+    /* End reset */
+
+    @import url(http://fonts.googleapis.com/css?family=Roboto:300);
+
+    #content {
+      font-family: Roboto;
+    }
+
+    @media screen and (max-width: 630px) {
+      .hide {display:none !important;}
+      .fullWidth {width:100% !important; height:auto !important;}
+      .center {text-align:center !important; width:100% !important; height:auto !important;}
+      .fullWidthSidePad {width:100% !important; padding:0 20px 0 20px;}
+    }
+
+  </style>
+</head>
+
+<body style="padding:0; margin:0">
+  <div id="content">
+    <p>TEST #2</p>
+  </div>
+</body>
+</html>
