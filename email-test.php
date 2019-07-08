0e5aa48da41417cb0ae008e84c53eb416e2472f0
RubixSolver99 <blackbattiger@outlook.com>
2019-07-07 19:07:43 -0500

    Trying an email sender

---
+++ email-test.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+if (isset($_POST["to"])) {
+  $_SESSION["email_to"] = $_POST["to"];
+  $_SESSION["email_from"] = $_POST["from"];
+  $_SESSION["email_sub"] = $_POST["sub"];
+  $_SESSION["email_message"] = $_POST["message"];
+  $_SESSION["email_title"] = $_POST["title"];
+  $_SESSION["email_redir"] = "http://www.furiousfalcons.org";
+  session_write_close();
+  header("Location: /sendemail.php");
+  exit();
+}
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+  <form method="post" action="email-test.php">
+    <input type="text" name="to"></input>
+    <input type="text" name="from"></input>
+    <input type="text" name="sub"></input>
+    <input type="text" name="message"></input>
+    <input type="text" name="title"></input>
+    <input type="submit"></input>
+  </form>
+</body>
+</html>
